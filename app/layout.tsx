e7b2936e75c1b279e03badc44d2d47573502204c
aitorzte-sketch <aitor.zte@gmail.com>
2026-02-01 21:04:46 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className={`${inter.className} bg-black text-white`}>{children}</body>
+    </html>
+  )
+}
