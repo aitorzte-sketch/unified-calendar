52ca9129e2358b0a3debc7817d0f6ece63e3d16c
aitorzte-sketch <aitor.zte@gmail.com>
2026-02-02 19:48:21 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Esto evita errores si Prisma no está listo durante el build
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
